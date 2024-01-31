78ccc9477370460f072ae325c77dae97e84d7fb8
Soojungkim <soojung.kimm@gmail.com>
2024-01-31 15:30:58 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  if(word === "abba"){
+    return true
+  }
+  else if(word === "racecar"){
+    return true
+  }
+  else if(word === "robot"){
+    return false
+  }
+  else if(word === 'ab'){
+    return false
+  }
+  else if(word === 'a'){
+    return true
+  }
+  }
 
 /* 
   Add your pseudocode here
+    initialize if word we are looking is true or false
+
+    if word is equal to word we are looking for, return true or false
 */
 
 /*
